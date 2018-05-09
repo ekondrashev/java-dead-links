5eef2d92d1f0f6dbf0ef6d259c64bba7f1210345
Denis <dwaynewade0033@gmail.com>
2018-05-09 13:48:36 +0300

    OutputTest

---
+++ src/test/java/DeadLinksTests.java
@@ -0,0 +1,52 @@
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class DeadLinksTests {
+
+
+    @Test
+    public void countBadLinks() throws Exception {
+        DeadLinks deadLinks = new SeleniumBadLinks();
+        List<String> links = deadLinks.badLinks("https://android.stackexchange.com/questions/4538/can-i-emulate-a-bluetooth-keyboard-with-my-android-device");
+        int result = 10;
+        assertEquals(result, links.size());
+    }
+
+    @Test
+    public void jsoupBadLinksTest() throws Exception {
+        DeadLinks deadLinks = new JsoupBadLinks();
+        List<String> links = deadLinks.badLinks("https://android.stackexchange.com/questions/4538/can-i-emulate-a-bluetooth-keyboard-with-my-android-device");
+        int result = 10;
+        assertEquals(result, links.size());
+    }
+
+    @Test
+    public void outputJsonTest()throws Exception{
+        DeadLinks deadLinks = new JsoupBadLinks();
+        List<String> links = deadLinks.badLinks("https://android.stackexchange.com/questions/4538/can-i-emulate-a-bluetooth-keyboard-with-my-android-device");
+String ex = "{\n" +
+        "  \"total:\" : ,\n" +
+        "  \"dead:\" : ,\n" +
+        "  \"404\" : {\n" +
+        "    \"urls:\" : [ [ \"\" ] ],\n" +
+        "    \"size:\" : \n" +
+        "  },\n" +
+        "  \"505\" : {\n" +
+        "    \"urls:\" : [ [ ] ],\n" +
+        "    \"size:\" : \n" +
+        "  },\n" +
+        "  \"url:\" : \"\"\n" +
+        "}";
+
+         String jsonExample = links.toString();
+
+        ex.equals(jsonExample);
+
+    }
+}

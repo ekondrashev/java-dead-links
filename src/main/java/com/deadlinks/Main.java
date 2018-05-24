0fd722b7a4a891f033c8624ffaaa0d2600b31a58
Marina Sirotkina <chmv7419@gmail.com>
2018-05-24 13:50:48 +0300

    step2 commit

--- src/main/java/com/deadlinks/Main.java
+++ src/main/java/com/deadlinks/Main.java
@@ -1,21 +1,8 @@
 package com.deadlinks;
 
-import com.deadlinks.model.DeadLinksReport;
-import com.deadlinks.service.DeadLinksService;
-import com.deadlinks.service.HttpClient;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class Main {
-    public static void main(String[] args) throws JsonProcessingException {
-        HttpClient httpClient = new HttpClient(args);
-        DeadLinksService deadLinksService = new DeadLinksService(httpClient);
-        DeadLinksReport deadLinksReport = deadLinksService.getDeadLinksReport();
-
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-
-        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(deadLinksReport));
+    public static void main(String[] args) {
+        Links html = new Links.HTML(args[0]);
+        System.out.println(html.toString());
     }
 }

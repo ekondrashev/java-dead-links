d7cee60eda1a2f0abfd4eec46d61e3fe50c81692
Andrey V <vandreyd1@gmail.com>
2018-05-29 00:31:28 +0300

    changes for step 2

---
+++ src/test/java/DeadLinksTest.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import org.junit.*;
+import org.junit.runner.*;
+import org.junit.runners.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Java code implementing the below contract; Test calling main(String[] args) and verifying the
+ * program system output correctness.
+ */
+
+@RunWith(JUnit4.class)
+public class DeadLinksTest {
+
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+  @Before
+  public void setUpStreams() {
+    System.setOut(new PrintStream(outContent));
+    System.setErr(new PrintStream(errContent));
+  }
+
+  @Test
+  public void out() {
+    String expectedResult = "{\n"
+        + "\t\"url\":\" http://roofing.tilda.ws\",\n"
+        + "\t\"404\": {\n"
+        + "\t\t\"size\": 1,\n"
+        + "\t\t\"urls\": [http://roofing.tilda.ws/somepage.html]\t\n"
+        + "},\n"
+        + "\t\"50x\": {\n"
+        + "\t\t\"size\": 0,\n"
+        + "\t\t\"urls\": []\t\n"
+        + "},\n"
+        + "\t\"dead\": 1,\n"
+        + "\t\"total\": 14\n"
+        + "}";
+    Main.main(new String[]{"http://roofing.tilda.ws"});
+    assertEquals(expectedResult.trim(), outContent.toString().trim());
+  }
+
+  @Test
+  public void err() {
+    String expectedError = "Illegal URL\n";
+    Main.main(new String[]{"asdasd"});
+    assertEquals(expectedError.trim(), errContent.toString().trim());
+  }
+
+  @After
+  public void restoreStreams() {
+    System.setOut(System.out);
+    System.setErr(System.err);
+  }
+
+}

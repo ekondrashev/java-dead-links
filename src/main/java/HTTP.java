9af599ee3648ac29e450a7f4e5e62714af78c52a
VAndreyD <vandreyd1@gmail.com>
2018-06-30 01:19:34 +0300

    path to the record file added into HTTP.Default

--- src/main/java/HTTP.java
+++ src/main/java/HTTP.java
@@ -14,58 +14,68 @@ interface HTTP {
 
     interface Response {
         int code();
+
         String asString();
 
     }
 
-  class Default implements HTTP {
+    class Default implements HTTP {
 
-    private HttpURLConnection connection;
-      private int responseCode;
-    private String responseMessage;
-
-    @Override
-    public Response response(URL url) {
-      return new Response() {
-        @Override
-        public int code() {
-          try {
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
-              responseMessage = connection.getContentType().split(";")[0];
-              responseCode = connection.getResponseCode();
-              if (Main.recordingJson != null) {
-                  this.recordIntoJson();
-              }
-              return responseCode;
-          } catch (IOException e) {
-            responseMessage = e.getMessage();
-            System.err.println("Connection Error:");
-            System.err.println(e.getMessage());
-            return -1;
-          }
-        }
+        private HttpURLConnection connection;
+        private int responseCode;
+        private String responseMessage;
+        private String recordPath;
 
         @Override
-        public String asString() {
-          return responseMessage;
+        public Response response(URL url) {
+            return new Response() {
+                @Override
+                public int code() {
+                    try {
+                        connection = (HttpURLConnection) url.openConnection();
+                        connection.setRequestMethod("GET");
+                        connection.connect();
+                        responseMessage = connection.getContentType().split(";")[0];
+                        responseCode = connection.getResponseCode();
+                        if (Main.recordingJson != null && recordPath != null) {
+                            this.recordIntoJson();
+                        }
+                        return responseCode;
+                    } catch (IOException e) {
+                        responseMessage = e.getMessage();
+                        System.err.println("Connection Error:");
+                        System.err.println(e.getMessage());
+                        return -1;
+                    }
+                }
+
+                @Override
+                public String asString() {
+                    return responseMessage;
+                }
+
+                private void recordIntoJson() {
+                    try (Writer writer = new FileWriter(recordPath + Main.recordingJson)) {
+                        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                        JsonObject recordingObject = new JsonObject();
+                        JsonObject urlJson = new JsonObject();
+                        urlJson.addProperty("code", responseCode);
+                        urlJson.addProperty("asString", responseMessage);
+                        recordingObject.add(url.toString(), urlJson);
+                        gson.toJson(recordingObject, writer);
+                    } catch (IOException ioe) {
+                        System.err.println(ioe.toString());
+                    }
+                }
+            };
         }
 
-          private void recordIntoJson() {
-              try (Writer writer = new FileWriter("src/test/resources/" + Main.recordingJson)) {
-                  Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                  JsonObject recordingObject = new JsonObject();
-                  JsonObject urlJson = new JsonObject();
-                  urlJson.addProperty("code", responseCode);
-                  urlJson.addProperty("asString", responseMessage);
-                  recordingObject.add(url.toString(), urlJson);
-                  gson.toJson(recordingObject, writer);
-          } catch (IOException ioe) {
-            System.err.println(ioe.toString());
-          }
+        public String getRecordPath() {
+            return recordPath;
         }
-      };
+
+        public void setRecordPath(String recordPath) {
+            this.recordPath = recordPath;
+        }
     }
-  }
 }

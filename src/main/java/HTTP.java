ac23d5f8c423bac8f7d9d0199874cd834f748cf1
Vladyslav Zasoba <vladzasoba16@gmail.com>
2018-07-04 17:27:00 +0300

    PR: Step5

--- src/main/java/HTTP.java
+++ src/main/java/HTTP.java
@@ -1,3 +1,9 @@
+import com.google.gson.*;
+import com.sun.org.apache.bcel.internal.util.ClassPath;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 interface HTTP {
@@ -7,6 +13,77 @@ interface HTTP {
     interface Response {
         int code();
         String asString();
+    }
+
+    class Default implements HTTP {
+        private boolean enableRecording = Main.enableRecordingJson;
+        private String recordingFileName = Main.JsonRecordingFileName;
+        private JsonArray jsonArray = new JsonArray();
+
+        {
+            if (enableRecording){
+                File fout;
+                if ((fout = new File(recordingFileName)).exists()) {
+                    fout.delete();
+                }
+            }
+        }
+
+        @Override
+        public Response response(URL url) {
+            return new Response() {
+                private int responseCode;
+                private String responseMessage;
+
+                @Override
+                public int code() {
+                    JSONObject jsonObject = new JSONObject();
+                    JSONObject jsonUrl = new JSONObject();
+                    try {
+                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                        responseCode = connection.getResponseCode();
+                        responseMessage = connection.getResponseMessage();
+
+                        if (enableRecording) {
+                            jsonUrl.put("code", responseCode);
+                            jsonUrl.put("asString", responseMessage);
+                            jsonObject.put(url.toString(), jsonUrl);
+                            recordJson(jsonObject);
+                        }
+                    } catch (IOException e) {
+                        responseMessage = "Invalid URL";
+                        if (enableRecording) {
+                            jsonUrl.put("code", 404);
+                            jsonUrl.put("asString", responseMessage);
+                            jsonObject.put(url.toString(), jsonUrl);
+                            recordJson(jsonObject);
+                        }
+                        return 404;
+                    }
+                    return responseCode;
+                }
+
+                @Override
+                public String asString() {
+                    return responseMessage;
+                }
+
+                public void recordJson(JSONObject jsonObject){
+                    JsonParser jsonParser = new JsonParser();
+                    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                    JsonElement el = jsonParser.parse(jsonObject.toString());
+                    jsonArray.add(el);
+                    String jsonOutput = gson.toJson(jsonArray);
+                    try {
+                        FileWriter fileWriter = new FileWriter(recordingFileName);
+                        fileWriter.write(jsonOutput);
+                        fileWriter.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+        }
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
 
 class Main {
+    public static boolean enableRecordingJson = true;
+    public static String JsonRecordingFileName;
+
     public static void main(String[] args) {
+        if (args.length > 1 && args[1].contains("--enable-json-recording=")) {
+            enableRecordingJson = true;
+            JsonRecordingFileName = args[1].substring(args[1].indexOf("=") + 1);
+        }
         Links html = new Links.HTML(args[0], new HTTP.Default());
         System.out.println(html.toString());
     }
